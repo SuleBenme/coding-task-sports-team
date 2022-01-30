fc1379f80f41a6e8dfcfc0106d25fe18bd2cb75b
Soulaimane Benmessaoud <s346178@oslomet.no>
2022-01-30 16:16:59 +0100

    adding types for match

--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,20 @@ export interface Competition {
   name: string;
   emblemUrl?: string;
 }
+export interface Match {
+  id: number;
+  names: Names;
+  utcDate: string;
+  result: Fulltime;
+  status: string;
+}
+
+interface Fulltime {
+  awayTeam?: number;
+  homeTeam?: number;
+}
+
+interface Names {
+  awayTeam: string;
+  homeTeam: string;
+}
