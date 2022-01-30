3b721db4ba637fb405b0e2a9245c281d7006a572
Soulaimane Benmessaoud <s346178@oslomet.no>
2022-01-30 21:05:35 +0100

    adding Link to competitions

--- src/Competitions/competition-card.tsx
+++ src/Competitions/competition-card.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { Competition } from "../types";
 import "./index.css";
 
@@ -5,13 +6,18 @@ function CompetitionCard({ code, name, id, emblemUrl }: Competition) {
   return (
     <div style={{ flex: "1 1 0" }}>
       <div className="wrapper-competiton-card">
-        <div className="container">
-          <div
-            className="background-image"
-            style={{ backgroundImage: `url(${emblemUrl}` }}
-          ></div>
-        </div>
-        <div className="competition-name">{name}</div>
+        <Link
+          style={{ color: "inherit", textDecoration: "inherit" }}
+          to={"competitions/" + id}
+        >
+          <div className="container">
+            <div
+              className="background-image"
+              style={{ backgroundImage: `url(${emblemUrl}` }}
+            ></div>
+          </div>
+          <div className="competition-name">{name}</div>
+        </Link>
       </div>
     </div>
   );
