a58555b3652f6c5355ebac40e8f7f6d96a398ac4
Soulaimane Benmessaoud <s346178@oslomet.no>
2022-01-30 20:56:15 +0100

    fixing style of competition card

--- src/Competitions/competition-card.tsx
+++ src/Competitions/competition-card.tsx
@@ -4,13 +4,15 @@ import "./index.css";
 function CompetitionCard({ code, name, id, emblemUrl }: Competition) {
   return (
     <div style={{ flex: "1 1 0" }}>
-      <div className="container">
-        <div
-          className="background-image"
-          style={{ backgroundImage: `url(${emblemUrl}` }}
-        ></div>
+      <div className="wrapper-competiton-card">
+        <div className="container">
+          <div
+            className="background-image"
+            style={{ backgroundImage: `url(${emblemUrl}` }}
+          ></div>
+        </div>
+        <div className="competition-name">{name}</div>
       </div>
-      <h4>{name}</h4>
     </div>
   );
 }
